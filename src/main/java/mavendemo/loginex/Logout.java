4ee5c463c5632b8909ff3f17292a6340ad0b93c5
Vishnu-CP <vishnu.7210@gmail.com>
2020-03-14 14:01:08 +0530

    LogoutServlet -Error rectification

--- src/main/java/mavendemo/loginex/Logout.java
+++ src/main/java/mavendemo/loginex/Logout.java
@@ -19,7 +19,7 @@ protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws Se
 	if(session != null){
 		session.removeAttribute("uname");
 		session.invalidate();
-		System.out.println("Session is deleted")
+		System.out.println("Session is deleted");
 	}
 	resp.sendRedirect("login.jsp");
 }
